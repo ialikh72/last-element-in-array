570fe157c1b891f0c01b2282f2b228563ce769e0
ialikh72 <ialikh.w1@gmail.com>
2026-02-22 22:40:13 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 4, 2, 7, 4, 9 };
+        Console.WriteLine("elements are:");
+        for(int i = 0; i < arr.Length; i++) 
+            Console.Write(arr[i]+" ");
+        Console.WriteLine();
+        Console.WriteLine("the last element is: "+arr[arr.Length - 1]);
+         
+    }
+}
